b9b80c3310a5739898782a6d47871a068d55e98a
guschins <guschins@gmail.com>
2023-04-15 16:19:34 +0200

    feat: add new module to material module

--- portfolio/src/app/material/material.module.ts
+++ portfolio/src/app/material/material.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCommonModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
+    MatCommonModule,
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatDividerModule,
     MatTabsModule,
     MatCardModule,
+    MatMenuModule,
   ],
   exports: [
     MatToolbarModule,
@@ -25,6 +27,7 @@ import { MatCardModule } from '@angular/material/card';
     MatDividerModule,
     MatTabsModule,
     MatCardModule,
+    MatMenuModule,
   ],
 })
 export class MaterialModule {}
