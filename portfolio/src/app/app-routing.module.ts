00853c5c27ab5cbb89e4b3806c7a6cff8a01f07d
guschins <guschins@gmail.com>
2023-04-15 16:28:01 +0200

    feat: add routing

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DescriptionsComponent } from './pages/descriptions/descriptions.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app-about', component: AboutComponent },
+  { path: 'app-projects', component: ProjectsComponent },
+  { path: 'app-contact', component: ContactComponent },
+  { path: 'app-descriptions', component: DescriptionsComponent },
+  { path: '', redirectTo: 'app-about', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
